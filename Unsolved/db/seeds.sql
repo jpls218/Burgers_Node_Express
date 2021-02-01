0de204723528cc6f70947abe79341d3ed90b309e
Jonathan Smith <jpls218@gmail.com>
2021-01-31 19:47:27 -0500

    database completed and added to MySql

---
+++ Unsolved/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ('Classic Hamburger');
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (name, devour) VALUES ('Bacon Cheese Burger', true);
